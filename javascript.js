7e69fe7e8b1944cec965b05ac7294d9cfad41014
klemenkac <xklemenx@gmail.com>
2015-05-15 22:34:58 +0200

    template2

--- javascript.js
+++
@@ -1,9 +0,0 @@
-var imgLoading = 'assets/images/lightbox/loading.gif';
-var imgClose = 'assets/images/lightbox/closelabel.gif';
-
-var videoWidth = 300;
-var videoHeight = 225;
-var youtubeID = 'FpAgUMUlhw4';
-
-var searchDefault = '';
-var searchColor = '#000000';
